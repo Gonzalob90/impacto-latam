c048144902ee3161c5369f4d6bcc7c8d3879b22a
System Administrator <root@Gonzalos-MacBook-Pro.local>
2016-12-07 21:43:02 -0500

    Intento de tener certificado

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 
+//Create Server
 var express = require('express');
 var app = express();
 
+//Previous configuaration
 var port = process.env.PORT || 3000;
 
 
 app.set('view engine', 'ejs');
+//New attempt
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+        res.redirect('https://' + req.headers.host + req.path);
+    }
+    else {
+        return next();
+    }
+});
+//
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -13,3 +25,10 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port);
+
+
+
+
+
+ 
+
