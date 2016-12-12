23fa7a64fa0e762095e3c42eaf9aeae8b05cfc30
System Administrator <root@Gonzalos-MacBook-Pro.local>
2016-12-12 16:08:22 -0500

    avance Angie, nav bar incompleto

--- app.js
+++ app.js
@@ -3,28 +3,18 @@
 var express = require('express');
 var app = express();
 
-//Previous configuaration
+//Previous configuration
 var port = process.env.PORT || 3000;
 
 
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
-//New attempt
-/*app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect('https://' + req.headers.host + req.path);
-    }
-    else {
-        return next();
-    }
+app.use('/', express.static(__dirname + '/public'));
+
+/*app.get('/', function(req, res) {
+	res.sendfile('./public/index.html');
 });
 */
-//
-app.use('/assets', express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-	res.render('index');
-});
 
 app.listen(port);
 
--- public/js/init.js
+++ public/js/init.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     	callback: mailChimpResponse,
 
     	// ADD YOUR MAILCHIMP URL BELOW HERE!
-	    url: 'http://kudosthemes.us8.list-manage1.com/subscribe/post?u=0ba0c2763eae356fe88ab9ef0&id=2c89c5db27'
+	    url: 'http://impactolatam.us14.list-manage.com/subscribe?u=6cde45932a8f2d9eb2053ff81&id=28cbf7ffe1'
 
 	});
 	function mailChimpResponse(resp) {
@@ -48,8 +48,8 @@ $(document).ready(function(){
 
 	    "use strict";
 
-	    // Date below denotes 23 April, 2015
-	    var launchDay = new Date(2015, 4-1, 23);
+	    // Date below denotes 15 April, 2015
+	    var launchDay = new Date(2017,1-1,21);
 	    $('#ticker').countdown({
 	    	until: launchDay,
 	    	layout: '<div class="col-sm-6 col-md-3"><div class="counter-bg"><div class="counter-inner"><span class="count-digit">{dn}</span><span class="count-text">{dl}</span></div></div></div><div class="col-sm-6 col-md-3"><div class="counter-bg"><div class="counter-inner"><span class="count-digit">{hn}</span><span class="count-text">{hl}</span></div></div></div><div class="col-sm-6 col-md-3"><div class="counter-bg"><div class="counter-inner"><span class="count-digit">{mn}</span><span class="count-text">{ml}</span></div></div></div><div class="col-sm-6 col-md-3"><div class="counter-bg"><div class="counter-inner"><span class="count-digit">{sn}</span><span class="count-text">{sl}</span></div></div></div>'
